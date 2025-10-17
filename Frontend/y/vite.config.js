77057ffcde586335fd3d7ba952dca9996d78a5d4
moataz <moatazbanmansour@gmail.com>
2025-10-17 07:28:15 +0100

    updated

--- Frontend/y/vite.config.js
+++ Frontend/y/vite.config.js
@@ -5,6 +5,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    build: {
+    cssMinify: 'esbuild' // Force Vite to use esbuild instead of lightningcss
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
